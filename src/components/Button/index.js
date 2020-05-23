1c22bab7df512defd528b28c1ec2e6ae47ab4c80
Artem Geroyan <geroyan.artem@mail.ru>
2020-05-23 19:29:51 +0300

    fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,6 +5,8 @@ import classNames from "classnames";
 import "./Button.scss";
 
 export default function Button(props) {
+console.log('BUTTON', props);
+
   return (
     <button {...props} className={classNames("button", props.className)}></button>
   );
